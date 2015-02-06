10961218a1b215a03246a6fcae8ee8364b00840d
Jonathan Cho & Vivek Ratkalkar <pair+joncho26.vratkalkar@devbootcamp.com>
2015-02-06 14:56:30 -0500

    add login and signup routes

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,7 +1,29 @@
 get '/login' do
-  erb
+ erb :'auth/login'
+end
+
+post '/login' do
+  @user = User.authenticate(params[:user][:email],params[:user][:password])
+  if @user
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    redirect '/login'
+  end
 end
 
 get '/signup' do
-
+  erb :'auth/signup'
 end
+
+post '/signup' do
+  @user = User.create(params[:user])
+  if @user
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    redirect '/signup'
+  end
+end
+
+

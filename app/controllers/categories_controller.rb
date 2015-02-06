f7621b4cfc604dd8b9bbc650ad20368a0a545627
gabalicious <gabalicious113@gmail.com>
2015-02-06 15:50:59 -0500

    fix bug with creation of new category in db

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,29 +1,27 @@
 get '/categories/new' do
-  erb :create_category
+  erb :category_form
 end
 
-post '/categories/new' do
-  erb :create_category
-  redirect '/categories/#{cat_id}'
+post '/categories' do
+  p params[:title]
+  new_category = Category.create(title: params[:title])
+  redirect "/categories/#{new_category.id}"
 end
 
-get 'categories/'
-	
-erb :all_categories
+get 'categories/' do
+	erb :all_categories
 end
 
 get '/categories/:category_id' do
-  erb :category
+  @current_category = Category.find_by(id: params[:category_id])
+
+  erb :current_category
+  #show all dream articles for category
 end
 
 put '/categories/:category_id' do
-  erb :category
+
 end
 
-get '/login' do
-  
-end
 
-get '/signup' do
-  
-end
+

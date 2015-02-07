93ca420ae18078632d434a2598ac0bce010dc59d
Jonathan Cho <joncho26@gmail.com>
2015-02-07 16:31:00 -0500

    add show all comments and new comment route

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,4 +1,13 @@
-get '/dreams/:id/comments' do
+get '/dreams/:id/comments/all' do
   @dream = Dream.find(params[:id])
   erb :'comment/all'
+end
+
+get '/comment/new' do
+  erb :'comment/new'
+end
+
+post '/comment' do
+  comment = Comment.create(params[:comment])
+  redirect "/dreams/#{comment.dream_id}/comments/all"
 end

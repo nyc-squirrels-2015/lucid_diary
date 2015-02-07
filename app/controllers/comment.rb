754923736d42a58e0ccacab5e7a0fd0bc640ad5c
Jonathan Cho <joncho26@gmail.com>
2015-02-07 18:27:55 -0500

    bad commit message

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,13 +1,13 @@
-get '/dreams/:id/comments' do
+get '/dreams/:id' do
   @dream = Dream.find(params[:id])
   erb :'comment/all'
 end
 
-get '/comment/new' do
+get '/dreams/:id/comment/new' do
   erb :'comment/new'
 end
 
 post '/comment' do
   comment = Comment.create(params[:comment])
-  redirect "/dreams/#{comment.dream_id}/comments/all"
+  redirect "/dreams/#{comment.dream_id}"
 end

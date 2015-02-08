c0a7ef6cbc132c955fd9691037b17c1a8b6640cb
Jonathan Cho <joncho26@gmail.com>
2015-02-08 14:28:17 -0500

    moved index test to separate file

--- spec/controller/auth_spec.rb
+++ spec/controller/auth_spec.rb
@@ -1,11 +1,6 @@
 require_relative '../spec_helper'
 
 describe "Auth" do
-  it 'should load an index page' do
-    get '/'
-    expect(last_response.status).to eq(200)
-  end
-
   it 'should have a login' do
     get '/'
     expect(last_response.body).to include("login")

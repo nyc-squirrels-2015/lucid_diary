6107ad8f73dcb11bd29611de6af03b60647c0d8f
Jonathan Cho <joncho26@gmail.com>
2015-02-08 18:53:18 -0500

    add signup tests

--- spec/controller/auth_spec.rb
+++ spec/controller/auth_spec.rb
@@ -1,6 +1,12 @@
 require_relative '../spec_helper'
 
 describe "Auth" do
+  let(:user) {User.create(name:'Steve Rogers',password:'password')}
+
+  before do
+    user
+  end
+
   it 'should have a login' do
     get '/'
     expect(last_response.body).to include("login")
@@ -9,5 +15,16 @@ describe "Auth" do
   it 'should have a signup' do
     get '/'
     expect(last_response.body).to include("signup")
+  end
+
+  it 'should respond to signup' do
+    post '/signup', params={ user:{name:'Steve Rogers',password:'password'}}
+    expect(last_response).to be_redirect
+    # p last_response
+    follow_redirect!
+    # p "*" * 40
+    # p last_response.body
+    # expect(last_response.body).to include("Steve Rogers")
+
   end
 end

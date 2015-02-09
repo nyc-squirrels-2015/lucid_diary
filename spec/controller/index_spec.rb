8abc8055ad484ebf87df0a6d89fbb821048645d6
Jonathan Cho <joncho26@gmail.com>
2015-02-08 19:02:16 -0500

    additional test

--- spec/controller/index_spec.rb
+++ spec/controller/index_spec.rb
@@ -3,5 +3,6 @@ describe "Index Spec" do
 it 'should load an index page' do
     get '/'
     expect(last_response.status).to eq(200)
+    expect(last_response.body).to include("Lucidity")
   end
 end

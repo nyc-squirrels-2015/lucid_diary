4be00748cc14e7c636b8fcfbb4bf81bd807be958
cpachomski <cpachomski@gmail.com>
2015-02-08 22:42:08 -0500

    finish dream creation form

--- app/controllers/dreams.rb
+++ app/controllers/dreams.rb
@@ -3,6 +3,7 @@ get '/categories/:category_id/dreams/new' do
 end
 
 post '/categories/:category_id/dreams/new' do
+  @category = Category.find_by(id: params[:category_id])
   @new_dream = Dream.create(title: params[:title], content: params[:content], user_id: session[:user_id], category_id: params[:category_id])
     redirect "/categories/#{@new_dream.category_id}/dreams/#{@new_dream.id}"
 end
